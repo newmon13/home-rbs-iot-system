d4de96b501ea8eccdc94fcb33a0515a73e18a51a
newmo13 <kubalipka123@gmail.com>
2025-03-03 12:54:11 +0100

    Initial commit

---
+++ src/main/java/zsl/homerbsiotsystem/HomeRbsIotSystemApplication.java
@@ -0,0 +1,13 @@
+package zsl.homerbsiotsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeRbsIotSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeRbsIotSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/zsl/homerbsiotsystem/HomeRbsIotSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package zsl.homerbsiotsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeRbsIotSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
